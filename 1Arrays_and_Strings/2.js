6b82fc7dae99a954a2abab7db7ca51ca47826346
Viet Vo <viet.allen.qvo@gmail.com>
2019-07-11 16:04:07 -0700

    started 1.2

---
+++ 1Arrays_and_Strings/2.js
@@ -0,0 +1,10 @@
+// Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+// Hash Tables (page 88)
+
+function permu(string1, string2) {
+  if (string1.length >= string2.length) {
+      baseWord = str
+  }
+}
+
+module.exports = permu;
